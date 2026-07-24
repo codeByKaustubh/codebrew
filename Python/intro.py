6cdf72991dbcd1dd1b9acdb3d5040ef5df91c18f
Sarthak Bhosale <154578679+Sarthakbhosle18@users.noreply.github.com>
2026-07-24 12:53:47 +0530

    new

---
+++ Python/intro.py
@@ -0,0 +1,15 @@
+print(12)
+
+
+num1=12
+print(num1)
+
+name:str = "Iron Man"
+age : int =21
+
+gender :str = 'male'
+
+print("my name is "+ name + ' I am '+ str (age)+" years old")
+print("my name is" ,name, 'I am ' + str (age)+ " years old")
+print("my name is {} I am {} years old".format(name,age))
+print(f"my name is {name}I am {age} years old")
