e3900bbba372a379c499d24a2451a8b11e594530
Krane <93140271+codeByKaustubh@users.noreply.github.com>
2026-09-22 06:56:16 +0000

    m

---
+++ Python/operators.py
@@ -0,0 +1,41 @@
+#operator
+num1 = 10
+num2 = 5
+
+# ctrl + /  comment
+#arithmetic operators
+# + - * / % ** //
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+#print(num1 / num2)
+#print(num1 % num2) remainder
+#print(num1 ** num2) exponential
+#print(num1 // num2) quoient
+
+#assignment operators
+# += -= *= /= %= **= //=
+#num1 += 5
+#num1 -= 5
+
+#conditional operators
+# > < >= <= == !=
+#print(num1 != num2)
+#print(num1 == num2)
+
+#logical operators
+# and or not (truth table)
+
+#print("AND :", num1 > num2 and num2 == 5)
+#print("OR :", num1 > num2 or num2 != 5)
+#print("NOT :", num1 > num2 and not (num2!== 5))
+
+#membership operators
+# in, not in
+#print("a" in "prasad")
+#print("q" in "prasad")
+
+#identity operators
+#is, is not
+#print (num1 is 10)
+#print (num1 is not 10)
